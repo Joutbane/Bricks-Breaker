30de2994ac98f94a6b9b3946b3e3ab1b48f87065
Joao <veirgal007@gmail.com>
2025-08-28 21:30:05 -0300

    Creating Menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/const.py
+++ code/const.py
@@ -1,7 +1,17 @@
 # C
-COLOR_BEIGE = (237, 232, 208)
+COLOR_CYAN = (0, 255, 255)
 COLOR_GREY = (137, 137, 137)
 COLOR_WHITE = (255, 255, 255)
+
+# E
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5
+}
 
 # M
 MENU_OPTION = ('START',
--- code/menu.py
+++ code/menu.py
@@ -4,7 +4,7 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.const import WIN_WIDTH, COLOR_WHITE, MENU_OPTION, COLOR_GREY
+from code.const import WIN_WIDTH, COLOR_WHITE, MENU_OPTION, COLOR_GREY, COLOR_CYAN
 
 
 class Menu:
@@ -14,15 +14,19 @@ class Menu:
         self.rect = self.surf.get_rect()
 
     def run(self, ):
+        menu_opition = 0
         pygame.mixer_music.load('./assets/SoundMenu.wav')
         pygame.mixer_music.play(-1)
         while True:
+            # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(100,"BRICKS BREAKER", (COLOR_GREY), (WIN_WIDTH / 2, 200))
 
-
             for i in range(len(MENU_OPTION)):
-                self.menu_text(75, MENU_OPTION[i], (COLOR_WHITE), (WIN_WIDTH / 2, 400 + 100 * i))
+                if i == menu_opition:
+                    self.menu_text(75, MENU_OPTION[i], (COLOR_CYAN), (WIN_WIDTH / 2, 400 + 100 * i))
+                else:
+                    self.menu_text(75, MENU_OPTION[i], (COLOR_WHITE), (WIN_WIDTH / 2, 400 + 100 * i))
 
             pygame.display.flip()
 
@@ -31,6 +35,21 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close screen
                     quit()  # end pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN: # DOWN KEY
+                         if menu_opition < len(MENU_OPTION) - 1:
+                             menu_opition += 1
+                         else:
+                            menu_opition = 0
+                    if event.key == pygame.K_UP: # UP KEY
+                         if menu_opition > 0:
+                             menu_opition -= 1
+                         else:
+                            menu_opition = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:  # ENTER
+                        return MENU_OPTION[menu_opition]
+
+
 
 
     def menu_text(self, text_size:int, text: str, text_color: tuple, text_center_pos: tuple  ):
